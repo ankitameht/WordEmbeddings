4d5ac9b2b7348e5906af74141d5af73682c333ea
Ankita Mehta <amehta02@pc.factset.com>
2018-08-21 16:56:11 +0530

    Completed vectorizer: Only Scikit Learn is fully functional.

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,6 +1,7 @@
 # with NLTK
 from collections import defaultdict
 from src.utils import tokenize
+from nltk.text import TextCollection
 
 def frequency_vectorize_nltk(doc):
     features = defaultdict(int)
@@ -15,9 +16,21 @@ def one_hot_vectorize_nltk(doc):
     }
     return vectors
 
+def tf_idf_vectorize_nltk(corpus):
+    print(corpus)
+    #corpus = [tokenize(doc) for doc in corpus]
+    texts  = TextCollection(corpus)
+    print(texts)
+    for doc in corpus:
+        yield {
+            term: texts.tf_idf(term, doc)
+            for term in doc
+        }
+
 #In Scikit-Learn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import Binarizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def frequency_vectorize_scikitLearn(corpus):
     vectorizer = CountVectorizer()
@@ -31,6 +44,11 @@ def one_hot_vectorize_scikitLearn(corpus):
     onehot = Binarizer()
     vector = onehot.fit_transform(corpus.toarray())
     return vector
+
+def tf_idf_vectorize_scikitLearn(corpus):
+    tfidf = TfidfVectorizer()
+    corpus = tfidf.fit_transform(corpus)
+    return corpus
 
 # The Gensim way
 import gensim
@@ -52,11 +70,18 @@ def one_hot_vectorize_gensim(corpus):
     ]
     return vectors
 
+def tf_idf_vectorize_gensim(corpus):
+    corpus = [tokenize(doc) for doc in corpus]
+    lexicon = gensim.corpora.Dictionary(corpus)
+    tfidf = gensim.models.TfidfModel(dictionary=lexicon, normalize=True)
+    vectors = [tfidf[lexicon.doc2bow(doc)] for doc in corpus]
+    return vectors
 
 if __name__ == "__main__":
     string_sample1 =  "What is the step by step guide to invest in share market in india?"
     string_sample2 = "What is the step by step guide to invest in share market?"
     corpus = [string_sample1, string_sample2]
+
 
     # Method 1
     vectors = map(frequency_vectorize_nltk, corpus)
@@ -82,4 +107,18 @@ if __name__ == "__main__":
 
     # Method 6
     vectors = one_hot_vectorize_gensim(corpus)
+    print(vectors)
+    
+    # Method 7
+    vectors = tf_idf_vectorize_nltk(corpus)
+    for v in vectors:
+        print(v)
+
+
+    # Method 8
+    vectors = tf_idf_vectorize_scikitLearn(corpus)
+    print(vectors)
+
+    # Method 9
+    vectors = tf_idf_vectorize_gensim(corpus)
     print(vectors)
